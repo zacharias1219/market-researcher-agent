d54c2e593baa417bf732f7ae4277263748a4c3ce
Richard Abishai <rickoshade1891@gmail.com>
2025-01-17 09:37:19 +0530

    Enhance crew management by integrating CompanyResearchTasks and improving logging functionality

--- backend/crew.py
+++ backend/crew.py
@@ -1,46 +1,51 @@
-from job_manager import append_event
+from datetime import datetime
+from typing import Callable
+from langchain_openai import ChatOpenAI
 from agents import CompanyResearchAgents
-from tasks import CompanyResearchTask
+from job_manager import append_event
+from tasks import CompanyResearchTasks
 from crewai import Crew
+
 
 class CompanyResearchCrew:
     def __init__(self, job_id: str):
         self.job_id = job_id
         self.crew = None
+        self.llm = ChatOpenAI(model="gpt-4-turbo-preview")
 
     def setup_crew(self, companies: list[str], positions: list[str]):
         agents = CompanyResearchAgents()
-        research_manager = agents.research_manager(companies, positions)
+        tasks = CompanyResearchTasks(
+            job_id=self.job_id)
+
+        research_manager = agents.research_manager(
+            companies, positions)
         company_research_agent = agents.company_research_agent()
 
-        tasks = CompanyResearchTask()
-
-        company_research_task = [
-            tasks.company_research(company_research_agent, company, positions) for company in companies
+        company_research_tasks = [
+            tasks.company_research(company_research_agent, company, positions)
+            for company in companies
         ]
 
-        manage_research = tasks.manager_research(research_manager, companies, positions, company_research_task)
+        manage_research_task = tasks.manage_research(
+            research_manager, companies, positions, company_research_tasks)
 
         self.crew = Crew(
-            name="Company Research Crew",
             agents=[research_manager, company_research_agent],
-            tasks=[*company_research_task, manage_research],
-            verbose=True
+            tasks=[*company_research_tasks, manage_research_task],
+            verbose=2,
         )
-        return 
 
     def kickoff(self):
         if not self.crew:
-            print(f"No crew found for {self.job_id}")
-            return
-        
-        append_event(self.job_id, "Starting crew")
+            append_event(self.job_id, "Crew not set up")
+            return "Crew not set up"
 
+        append_event(self.job_id, "Task Started")
         try:
-            print(f"Running crew for {self.job_id}")
             results = self.crew.kickoff()
-            append_event(self.job_id, "Crew finished")
+            append_event(self.job_id, "Task Complete")
             return results
-        
         except Exception as e:
+            append_event(self.job_id, f"An error occurred: {e}")
             return str(e)
